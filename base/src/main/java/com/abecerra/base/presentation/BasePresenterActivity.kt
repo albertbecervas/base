d8e720ff9b9845a539d883f9730bdc501874fa53
albertbecerra <albertbecerrahervas@gmail.com>
2020-03-02 13:19:05 +0100

    refactor BaseActivity

--- base/src/main/java/com/abecerra/base/presentation/BasePresenterActivity.kt
+++ base/src/main/java/com/abecerra/base/presentation/BasePresenterActivity.kt
@@ -1,16 +1,20 @@
 package com.abecerra.base.presentation
 
 import android.os.Bundle
-import android.os.Parcelable
 import com.abecerra.base.PresenterConfigurator
 
-abstract class BasePresenterActivity<PRESENTER : Parcelable> : BaseActivity() {
+abstract class BasePresenterActivity<PRESENTER> : BaseActivity() {
 
     protected var presenter: PRESENTER? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        presenter = intent?.getParcelableExtra<PresenterConfigurator<PRESENTER>>("config")
-            ?.getPresenter()
+        presenter = intent?.getParcelableExtra<PresenterConfigurator<PRESENTER>>(
+            PRESENTER_CONFIGURATOR_KEY
+        )?.getPresenter()
+    }
+
+    companion object {
+        const val PRESENTER_CONFIGURATOR_KEY: String = "presenter_configurator"
     }
 }
